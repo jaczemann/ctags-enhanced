7c2fb35e17b8c869032b59ae2cba7ccdabc9a905
jaczemann <75168931+jaczemann@users.noreply.github.com>
2024-03-18 23:06:10 +0000

    Add files via upload

--- analyze.py
+++ analyze.py
@@ -1,7 +1,42 @@
 import re
 import ast
+import os
+from collections import defaultdict
 
 true = True
+
+def createPathNavigation():
+	navig=[]
+	for root, dirs, files in os.walk(os.getcwd()):
+		navig.append([root.replace(r'/opt', ''), dirs])
+
+	return navig
+
+navig = createPathNavigation()
+
+
+
+with open("deps", "r") as deps_file:
+	dps = deps_file.readlines()
+	for i, line in enumerate(dps):
+		if "#include" not in line:
+			dps[i] = ' '
+			continue
+		dps[i] = dps[i].replace('\n', '')
+		dps[i] = dps[i].split(":")
+		dps[i][1] = dps[i][1].split(" ")[1]
+
+	files=[]
+	for i in dps:
+		files.append(i[0])
+	set(files)
+	deps = {file: [] for file in files}
+
+	for dependency in dps:
+		deps[dependency[0]].append(dependency[1])
+
+
+#TODO: process deps
 
 
 with open("tags", "r") as tags_file:
@@ -16,7 +51,6 @@ with open("tags", "r") as tags_file:
 	tgs = tgs.replace("true", "True").replace("false", "False")
 	tgs = "[" + str(tgs) + "]"
 	tgs = tgs.replace('}, ]', '} ]')
-	print(tgs)
 
 	tags = ast.literal_eval(tgs)
 
@@ -42,13 +76,11 @@ for tag in tags:
 
 
 		if ':' in tags_proc[tags.index(tag)]['typeref']:
-			print(tags_proc[tags.index(tag)]['typeref'])
 			tags_proc[tags.index(tag)].update({'typeref': tags_proc[tags.index(tag)]['typeref'].split(":")[1]})
 
 	if (tag['kind'] == 'variable'):
 		txt = tag['typeref']
 		if ':' in tags_proc[tags.index(tag)]['typeref']:
-			print(tags_proc[tags.index(tag)]['typeref'])
 			tags_proc[tags.index(tag)].update({'typeref': tags_proc[tags.index(tag)]['typeref'].split(":")[1]})
 
 	if (tag['kind'] == 'enumerator'):
@@ -60,7 +92,6 @@ for tag in tags:
 
 		tags_proc[tags.index(parent)]['pattern'].append(tag['pattern'].replace("/^\t", "").replace("$/", "").replace(",", ""))
 
-print(tags_proc)
 
 tags_group = {"files": []}
 file_struct = { "variables":[], "functions":[], "types": [], "enums": [] }
@@ -69,7 +100,8 @@ file_struct = { "variables":[], "functions":[], "types": [], "enums": [] }
 source_files = list(set([tag['path'] for tag in tags_proc]))
 
 for src in source_files:
-	tags_group['files'].append(dict({src: file_struct}))
+	#tags_group['files'].append(dict({src: file_struct})) #FIXME
+	tags_group['files'].append({src: file_struct})
 
 	for tag in tags_proc:
 		if tag['path'] == src:
@@ -84,3 +116,28 @@ for src in source_files:
 
 print("\n\n\n")
 print(tags_group)
+
+
+
+def createBlueprint():
+	with open("blueprint", "w") as blueprint:
+		blueprint.write("BEGIN\n")
+
+	bp_data = []
+
+	bp_data.append("NEW\tROOT\t" + "RootName" + "\n")
+	for path, create in navig:
+		bp_data.append("NEW\tPACKAGE\t" + path + "\t" + ';'.join(create) + "\n")
+
+	bp_data.append("NEW\tDIAGRAM\tDiagramName\n")
+	bp_data.append("GOTO\tDIAGRAM\tDiagramName\n")
+
+	for file in source_files:
+		bp_data.append("NEW\tMODULE\t" + file.replace('.c', '') + "\n")
+
+
+	bp_data.append("END\n")
+	with open("blueprint", "a") as blueprint:
+		blueprint.writelines(bp_data)
+
+createBlueprint()
--- run_analyze.sh
+++ run_analyze.sh
@@ -1,3 +1,7 @@
 rm tags > /dev/null
+[[ ! -z $1 ]] && pwd=$1
+pwd=$pwd
 ctags --output-format=json --language-force=C --append --recurse -o tags
-python /opt/python/analyze.py
+ls --recursive | grep ":" > dirs
+grep -r "#include" > deps
+python -i /opt/python/analyze.py
